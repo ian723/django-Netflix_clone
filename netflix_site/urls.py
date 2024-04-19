722eeb29f314bd7531a0c4f8e3bb517c5fe38424
Ian Ndegwa <148945932+ian723@users.noreply.github.com>
2024-04-19 09:23:46 +0300

    Add files via upload

---
+++ netflix_site/urls.py
@@ -0,0 +1,20 @@
+"""
+URL configuration for netflix_site project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views .urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('core.urls'))
+]
+
+urlpatterns = urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
